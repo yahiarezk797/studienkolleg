14b30f9fae26df9e629039e2cc6d35f33b23f7f2
yahiarezk797 <yahiarezk797@gmail.com>
2026-03-24 12:19:00 +0200

    finall

--- main.py
+++ main.py
@@ -6,58 +6,63 @@ ph = PasswordHasher()
 
 
 def main():
-    print("Willkommen!")
-    print("1: Anmelden")
-    print("2: Konto eröffnen")
-    x = int(input("? "))
-    if x == 1:
-        id = input("ID: ")
-        name = input("Name: ")
-        pwd = input("Passwort: ")
-        konto = anmelden(id, name, pwd)
-        print(konto)
-        print(f"Hallo, Frau/Herr {konto.name}\n")
-        while True:
-            print(f"Geld: {konto.geld}")
-            print("1: Geld abheben")
-            print("2: Geld überweisen")
-            print("3: Geld einzahlen")
-            print("4: Kontoverlauf schauen")
-            print("5: Konto verlassen")
-            y = int(input("? "))
-            if y == 1:
-                betrag = int(input("Betrag: "))
-                geld_abheben(konto, betrag)
-            elif y == 2:
-                betrag = int(input("Betrag: "))
-                id2 = int(input("ID: "))
-                konto2 = id_to_konto(id2)
-                geld_überweisen(konto, konto2, betrag)
-            elif y == 3:
-                betrag = int(input("Betrag: "))
-                geld_legen(konto, betrag)
-            elif y == 4:
-                kontoverlauf_schauen(konto)
-            elif y == 5:
-                break
-            else:
-                print("Ungültige Anfrage!")
-            
+    while True:
+        print("Willkommen!")
+        print("1: Anmelden")
+        print("2: Konto eröffnen")
+        print("3: Program verlassen")
+        x = int(input("? "))
+        if x == 1:
+            id = input("ID: ")
+            name = input("Name: ")
+            pwd = input("Passwort: ")
+            konto = anmelden(id, name, pwd)
+            print(konto)
+            print(f"Hallo, Frau/Herr {konto.name}\n")
+            while True:
+                print(f"Geld: {konto.geld}")
+                print("1: Geld abheben")
+                print("2: Geld überweisen")
+                print("3: Geld einzahlen")
+                print("4: Kontoverlauf schauen")
+                print("5: Konto verlassen")
+                y = int(input("? "))
+                if y == 1:
+                    betrag = int(input("Betrag: "))
+                    geld_abheben(konto, betrag)
+                elif y == 2:
+                    betrag = int(input("Betrag: "))
+                    id2 = int(input("ID: "))
+                    konto2 = id_to_konto(id2)
+                    geld_überweisen(konto, konto2, betrag)
+                elif y == 3:
+                    betrag = int(input("Betrag: "))
+                    geld_legen(konto, betrag)
+                elif y == 4:
+                    kontoverlauf_schauen(konto)
+                elif y == 5:
+                    break
+                else:
+                    print("Ungültige Anfrage!")
+                
 
-    elif x == 2:
-        name = input("Name: ")
-        pwd1 = input("Passwort: ")
-        pwd2 = input("Passwort bestätigen: ")
-        if pwd2 != pwd1:
-            print("Paswort nicht gleich!")
-            return
-        pwd = ph.hash(pwd1)
+        elif x == 2:
+            name = input("Name: ")
+            pwd1 = input("Passwort: ")
+            pwd2 = input("Passwort bestätigen: ")
+            if pwd2 != pwd1:
+                print("Paswort nicht gleich!")
+                return
+            pwd = ph.hash(pwd1)
 
-        betrag = input("Betrag: ") 
-        print(konto_eröffnen(name, pwd, betrag))
+            betrag = input("Betrag: ") 
+            print(konto_eröffnen(name, pwd, betrag))
+        
+        elif x == 3:
+            break
 
-    else:
-        print("Ungültige Anfrage!")
+        else:
+            print("Ungültige Anfrage!")
 
 
 if __name__ == "__main__":
